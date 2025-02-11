9e41aadac728a03a8313360f519597a8a042a1fa
emailnikola <emailnikola@gmail.com>
2025-02-11 01:04:51 +0100

    More tests for QuizQuestion

--- tests/unit/components/QuizQuestion.spec.ts
+++ tests/unit/components/QuizQuestion.spec.ts
@@ -5,14 +5,15 @@ import {
   expect,
   it,
   beforeEach,
-  afterEach
+  afterEach,
+  vi
 } from 'vitest'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify'
 import QuizQuestion from '@/components/QuizQuestion.vue'
 
 describe('QuizQuestion', () => {
-  let wrapper: VueWrapper
+  let wrapper: VueWrapper<any>
 
   beforeEach(() => {
     wrapper = mount(
@@ -38,8 +39,48 @@ describe('QuizQuestion', () => {
     wrapper.unmount()
   })
 
-  it('Should contain menu button', () => {
-    const menuBtn = wrapper.find('[data-testid="apq-quiz-question"]')
-    expect(menuBtn.exists()).toBe(true)
+  it('Should render component', () => {
+    const componentWrapper = wrapper.find('[data-testid="apq-quiz-question"]')
+    expect(componentWrapper.exists()).toBe(true)
+  })
+  
+  it('should calculate the correct visibleQuestionIndex based on scrollFromTop', async () => {
+    await wrapper.setProps({ scrollFromTop: 600 })
+    const visibleQuestionIndex = wrapper.vm.visibleQuestionIndex
+    expect(visibleQuestionIndex).toBe(0)
+  })
+
+  it('should calculate the correct hideQuestionPercent value', async () => {
+    const hidePercent = wrapper.vm.hideQuestionPercent()
+    expect(hidePercent).toBeGreaterThanOrEqual(0)
+  })
+
+  it('should calculate the correct revealQuestionPercent value', async () => {
+    const revealPercent = wrapper.vm.revealQuestionPercent()
+    expect(revealPercent).toBeGreaterThanOrEqual(0)
+  })
+
+  it('should emit "option-selected" event when an option is clicked', async () => {
+    const option = 'option_1'
+    const handleClickSpy = vi.spyOn(wrapper.vm, 'handleClick')
+
+    const optionCard = wrapper.findAll('.apq-option').at(0)
+    await optionCard?.trigger('click')
+
+    expect(handleClickSpy).toHaveBeenCalledWith(option)
+    expect(wrapper.emitted()['option-selected'][0]).toEqual([{ value: option, position: 1 }])
+  })
+
+  it('should apply dynamic background image classes based on questionPosition', () => {
+    const positionClass = wrapper.classes().find((className) =>
+      className.startsWith('apq-background-image')
+    )
+    expect(positionClass).toBe('apq-background-image_1')
+  })
+
+  it('should have the correct style for the clip-path based on scrollFromTop', async () => {
+    await wrapper.setProps({ scrollFromTop: 200 })
+    const style = wrapper.attributes('style')
+    expect(style).toContain('clip-path: polygon(')
   })
 })
